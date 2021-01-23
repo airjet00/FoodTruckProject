99cbe77e138c6fa14fac463743dd24622b4ec722
erikskell <erikskell@gmail.com>
2021-01-22 23:04:11 -0600

    working

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -2,4 +2,65 @@ package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
 
+	/* fields for a unique numeric id, a name ("TacoRific", 
+	 * Mediterranean Medic", etc), food type ("Tacos", "Falafel"
+	 * etc.), and numeric rating
+	 */
+	private String truckName;
+	private String foodType;
+	private int rating;
+	
+	void FoodTruck(String truckName, String foodType, 
+					int rating, int truckId) {
+		//truck id  assigned
+		
+	}
+
+	/**
+	 * @return the truckName
+	 */
+	public String getTruckName() {
+		return truckName;
+	}
+
+	/**
+	 * @param truckName the truckName to set
+	 */
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+	/**
+	 * @return the foodType
+	 */
+	public String getFoodType() {
+		return foodType;
+	}
+
+	/**
+	 * @param foodType the foodType to set
+	 */
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	/**
+	 * @return the rating
+	 */
+	public int getRating() {
+		return rating;
+	}
+
+	/**
+	 * @param rating the rating to set
+	 */
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString() {
+		return "FoodTruck [truckName=" + truckName + ", foodType=" + foodType + ", rating=" + rating + "]";
+	}
+	
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -3,8 +3,42 @@ package com.skilldistillery.foodtruck;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//array to store up to five food trucks objects
+		//call run method
 	}
 
+	//method to run the app
+	
+		/*
+		 * user prompt to input name, food type, and rating
+		 * for up to five food trucks. For each set of input
+		 * a FoodTruck object is created, its fields set the 
+		 * user's input, and it is added to the array
+		 * *** Truck id is NOT input by the user, but
+		 * assigned automatically in the FoodTruck constructor
+		 * 
+		 */
+
+	
+	/*
+	 * If the user inputs quit for the food truck name, input 
+	 * ends immediately and the program continues.
+	 */
+
+	
+	/*
+	 * After input is complete, the user sees a menu from which 
+	 * they can choose to:
+
+		List all existing food trucks.
+		See the average rating of food trucks.
+		Display the highest-rated food truck.
+		Quit the program.
+	 */
+	
+	/*
+	 * After choosing a menu item, the user sees the menu again 
+	 * and can choose another item until the choose to quit.
+	 */
+			
 }
